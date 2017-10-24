118c8121679a6ec63900d0e401ed935bc09d09fc
龔育晴 <ching85117@gmail.com>
2017-10-24 17:53:57 +0800

    驗證，修改route中的/task部分

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,22 @@ use Illuminate\Http\Request;
      */
 Route::get('/', function () {
     return view('tasks');
-});
+    });
     /**
      * 增加新的任務
      */
     Route::post('/task', function (Request $request) {
-        //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // 建立該任務...
     });
 
     /**
@@ -24,3 +34,4 @@ Route::get('/', function () {
     Route::delete('/task/{task}', function (Task $task) {
         //
     });
+
