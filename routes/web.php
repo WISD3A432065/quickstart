bfd6432999cfea6209f943bb9a63f1ecbe3312b7
龔育晴 <ching85117@gmail.com>
2017-10-24 17:46:35 +0800

    修改route，顯示視圖

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,9 @@ use Illuminate\Http\Request;
     /**
      * 顯示所有任務
      */
-    Route::get('/', function () {
-        //
-    });
-
+Route::get('/', function () {
+    return view('tasks');
+});
     /**
      * 增加新的任務
      */
