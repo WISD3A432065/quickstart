ade2476e6bf416df1aaa78c50c0b40f53f16d003
龔育晴 <ching85117@gmail.com>
2017-10-24 17:58:14 +0800

    顯示已有的任務

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,13 @@ use Illuminate\Http\Request;
     /**
      * 顯示所有任務
      */
-Route::get('/', function () {
-    return view('tasks');
-    });
+    Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
     /**
      * 增加新的任務
      */
